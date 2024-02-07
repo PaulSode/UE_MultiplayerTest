34c323a060e8d8574fc0e9d05f1b37d5682fe53d
NihachiOokami <paulsode05@gmail.com>
2024-02-07 16:48:13 -0500

    feat: Working sign up, log in, and recovery systems using Azure Playfab

--- Source/TestMultiplayer/TestMultiplayer.Build.cs
+++ Source/TestMultiplayer/TestMultiplayer.Build.cs
@@ -8,6 +8,6 @@ public class TestMultiplayer : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput", "PlayFab", "PlayFabCpp", "PlayFabCommon" });
 	}
 }
